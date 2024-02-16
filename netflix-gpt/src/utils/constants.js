22fe99f515e62797b78d3fb247a2f9b76f890444
ruchita <ruchitasagalgile04@gmail.com>
2024-02-16 06:31:32 +0530

    env changes

--- netflix-gpt/src/utils/constants.js
+++ netflix-gpt/src/utils/constants.js
@@ -1,3 +1,5 @@
+export const OPENAI_KEY = process.env.REACT_APP_OPENAI_KEY;
+
 export const LOGO =
   "https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png";
 
@@ -22,5 +24,3 @@ export const SUPPORTED_LANGUAGES = [
   { identifier: "hindi", name: "Hindi" },
   { identifier: "spanish", name: "Spanish" },
 ];
-
-export const OPENAI_KEY = "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";
